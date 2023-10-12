84503f8d188d6bdd120c24d9c68adcbec583820d
Kruzikael014 <marchelhermanliansyah@gmail.com>
2023-10-12 15:05:33 +0700

    12/10/23 - benerin form, lengkapin sesuai model

--- lib/models/profile/profile.dart
+++ lib/models/profile/profile.dart
@@ -25,7 +25,8 @@ class Profile {
   final String location;
   final String? introduction;
   final List<String>? tag;
-  final List<SocialMedia>? socialMedia;
+  final List<SocialMedia> socialMedia;
+  final String role;
 
   Profile({
     required this.authID,
@@ -37,23 +38,27 @@ class Profile {
     required this.location,
     this.introduction,
     this.tag,
-    this.socialMedia,
+    required this.socialMedia,
+    required this.role,
   });
 
-  // factory Profile.fromJson(Map<String, dynamic> json) {
-  //   return Profile(
-  //     authID: json['authID'],
-  //     firstName: json['firstName'],
-  //     lastName: json['lastName'],
-  //     profileImage: json['profileImage'],
-  //     gender: json['gender'],
-  //     dob: DateTime.parse(json['dob']),
-  //     location: json['location'],
-  //     introduction: json['introduction'],
-  //     tag: List<String>.from(json['tag']),
-  //     socialMedia: (json['socialMedia'] as List<dynamic>)
-  //         .map((media) => SocialMedia.fromJson(media))
-  //         .toList(),
-  //   );
-  // }
+  factory Profile.fromJson(Map<String, dynamic> json) {
+    return Profile(
+      authID: json['authID'],
+      firstName: json['firstName'],
+      lastName: json['lastName'] ? json['lastName'] : '',
+      profileImage: json['profileImage'] ? json['profileImage'] : '',
+      gender: json['gender'],
+      dob: DateTime.parse(json['dob']),
+      location: json['location'],
+      introduction: json['introduction'] ? json['introduction'] : '',
+      tag: List<String>.from(json['tag']).isNotEmpty
+          ? List<String>.from(json['tag'])
+          : <String>[],
+      socialMedia: (json['socialMedia'] as List<dynamic>)
+          .map((media) => SocialMedia.fromJson(media))
+          .toList(),
+      role: json['role'],
+    );
+  }
 }
--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -14,58 +14,147 @@ class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(20),
-        child: Column(
-          children: [
-            SizedBox(
-              height: 10,
-            ),
-            Center(
-              child: Text(
-                'Profile',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 10,
+              ),
+              Center(
+                child: Text(
+                  'Profile',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Center(
-              child: CircleAvatar(
-                backgroundImage: NetworkImage(
-                  'https://firebasestorage.googleapis.com/v0/b/temu-4793d.appspot.com/o/profile_images%2FS__21184514.jpg?alt=media&token=d52768ee-b599-4be4-bad9-c31948d3e313&_gl=1*1iad3y3*_ga*MTc4Nzc0NDIwMC4xNjk2MjEwNTAz*_ga_CW55HF8NVT*MTY5NjgzNjc0Ni45LjEuMTY5NjgzNzAyMS4xOS4wLjA.',
-                  scale: 1,
-                ),
-                radius: 64,
+              SizedBox(
+                height: 20,
               ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    decoration: InputDecoration(
-                      hintText: 'FirstName',
-                      label: Text('FirstName'),
+              Center(
+                child: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                    'https://firebasestorage.googleapis.com/v0/b/temu-4793d.appspot.com/o/profile_images%2FS__21184514.jpg?alt=media&token=d52768ee-b599-4be4-bad9-c31948d3e313&_gl=1*1iad3y3*_ga*MTc4Nzc0NDIwMC4xNjk2MjEwNTAz*_ga_CW55HF8NVT*MTY5NjgzNjc0Ni45LjEuMTY5NjgzNzAyMS4xOS4wLjA.',
+                    scale: 1,
+                  ),
+                  radius: 64,
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        label: Text('FirstName'),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Color(AppColor.text),
+                            style: BorderStyle.solid,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        label: Text('LastName'),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Color(AppColor.text),
+                            style: BorderStyle.solid,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextField(
+                minLines: 4,
+                maxLines: 5,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Color(AppColor.text),
+                      style: BorderStyle.solid,
+                    ),
+                  ),
+                  label: Text('Introduction'),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  label: Text('Role'),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Color(AppColor.text),
+                      style: BorderStyle.solid,
                     ),
                   ),
                 ),
-                Expanded(
-                  child: TextField(
-                    decoration: InputDecoration(
-                      hintText: 'LastName',
-                      label: Text('LastName'),
+                enabled: false,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  label: Text('Gender'),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Color(AppColor.text),
+                      style: BorderStyle.solid,
                     ),
                   ),
-                )
-              ],
-            ),
-          ],
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  label: Text('Location'),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Color(AppColor.text),
+                      style: BorderStyle.solid,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  label: Text('Date of Birth'),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Color(AppColor.text),
+                      style: BorderStyle.solid,
+                    ),
+                  ),
+                ),
+                enabled: false,
+              ),
+            ],
+          ),
         ),
       ),
       backgroundColor: Color(AppColor.background),
